45ff47e6949660b24129a0eb03d9d77f8bb5cc3a
gustavo <kg.2k.pl@gmail.com>
2025-05-18 17:30:33 -0400

    Refactor getCryptos function and update type definitions for better clarity and structure

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,28 @@
 import axios from 'axios';
-import {create} from 'zustand';
+import { create } from 'zustand';
 import { CryptoCurrenciesResponseArraySchema } from './schema/crypto-schema';
+import type { Cryptocurrency } from './types';
+
+type CryptoStore = {
+    cryptocurrencies: Cryptocurrency[]
+    fetchCriptos: () => Promise<void>
+}
 
 async function getCryptos() {
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
-    const {data: {Data}} = await axios(url)
+    const { data: { Data } } = await axios(url)
     const result = CryptoCurrenciesResponseArraySchema.safeParse(Data)
-    console.log(result)
+    if (result.success) {
+        return result.data
+    }
 }
 
-export const useCryptoStore = create(() => ({
-    fetchCriptos: () => {
-        getCryptos()
+export const useCryptoStore = create<CryptoStore>((set) => ({
+    cryptocurrencies: [],
+    fetchCriptos: async () => {
+        const cryptocurrencies = await getCryptos()
+        set(() => ({
+            cryptocurrencies
+        }))
     }
 }))
